27f343d167f581dbd87f8f4bca1b6ab30b269916
CalinP1 <prostean.vasilecalin@gmail.com>
2024-01-18 19:20:45 +0200

    done initial hero page + basic css fot the hero page

--- src/pages/Heropage.jsx
+++ src/pages/Heropage.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import Pagelist from "../components/Pagelist";
 import Footer from "../components/Footer";
 import styles from "./Heropage.module.css";
+import { NavLink } from "react-router-dom";
 
 function Heropage() {
   return (
@@ -9,16 +10,60 @@ function Heropage() {
       <Navbar />
       <Pagelist />
       <div className={styles.description}>
-        <img
-          src="../../images/cooking-2.png"
-          alt="food-banner"
-          className={styles.foodbanner}
-        />
-        <img src="../../images/food-1.png" />
-        <img src="../../images/food-1.png" />
-        <img src="../../images/food-1.png" />
-        <img src="../../images/food-1.png" />
-        <img src="../../images/food-1.png" />
+        <div className={styles.foodbanner}>
+          <h1 className={styles.foodmoto}>
+            Do it yourself or find someone to do it!
+          </h1>
+        </div>
+        <div className={styles.herodescription}>
+          <h2 className={styles.herodescriptiontext}>
+            Some text to understand what the app does and what are the benefits
+            of making an account
+          </h2>
+        </div>
+        <h3 className={styles.titleForLogIn}>
+          Best way of enjoing FoodingAround? Create an account!
+        </h3>
+        <div className={styles.loginGuidanceHeroContainer}>
+          <div className={styles.loginGuidanceHero}>
+            <img
+              src="../../images/create-an-account-hero.png"
+              alt="log-in-exemple"
+              className={styles.logInHeroImage}
+            />
+            <p className={styles.logInExplanation}>
+              Here is an explanation on how your experience using this site will
+              look if you create an account
+            </p>
+          </div>
+          <div className={styles.loginGuidanceHero}>
+            <p className={styles.logInExplanation}>
+              Anothere example of text.. Here is an explanation on how your
+              experience using this site will look if you create an account
+            </p>
+            <img
+              src="../../images/food-1.png"
+              alt="log-in-exemple"
+              className={styles.logInHeroImage}
+            />
+          </div>
+          <div className={styles.loginGuidanceHero}>
+            <img
+              src="../../images/food-1.png"
+              alt="log-in-exemple"
+              className={styles.logInHeroImage}
+            />
+            <p className={styles.logInExplanation}>
+              Aaaaaand anothere one... Here is an explanation on how your
+              experience using this site will look if you create an account
+            </p>
+          </div>
+        </div>
+        <div className={styles.createAnAccountButton}>
+          <NavLink to="/login" className={styles.heroButton}>
+            CREATE AN ACCOUNT NOW
+          </NavLink>
+        </div>
       </div>
       <Footer />
     </div>
