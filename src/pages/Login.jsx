b45c785ccab5a6f87bfed5d52db1ec3712488d76
CalinP1 <prostean.vasilecalin@gmail.com>
2024-01-21 15:28:48 +0200

    Log-In page + Log-in styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,26 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import styles from "./Login.module.css";
 function Login() {
   return (
     <div>
       <Navbar />
+      <div className={styles.logInContainer}>
+        <h1 className={styles.logInHeading}>Sign in</h1>
+        <form className={styles.formContainer}>
+          <input
+            type="email"
+            placeholder="Email"
+            className={styles.inputLogin}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className={styles.inputLogin}
+          />
+          <button className={styles.logInButton}>Log In</button>
+        </form>
+      </div>
 
       <Footer />
     </div>
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,12 +1,19 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Pagelist from "../components/Pagelist";
+import styles from "./Recipes.module.css";
 
 function Recipes() {
   return (
-    <div>
+    <div className={styles.recipesBody}>
       <Navbar />
       <Pagelist />
+      <h1>Find the perfect meal to cook</h1>
+      <h2>
+        You can search by cuisine, ingredients, nutrients... or find your new
+        favourite dish by getting a random recipe
+      </h2>
+
       <Footer />
     </div>
   );
